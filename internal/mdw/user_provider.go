b5434e8832f59c9a2d31bb3d6e43d513b2460dbc
Arsolitt <arsolitt@gmail.com>
2025-12-09 18:41:03 +0300

    Add user provider middleware for Telegram updates
    Implement UserProvider middleware to handle user data from Telegram updates, including user creation and locking mechanisms. This middleware integrates with the user repository and enhances logging for user-related operations.

---
+++ internal/mdw/user_provider.go
@@ -0,0 +1,137 @@
+package mdw
+
+import (
+	"errors"
+	"log/slog"
+	"minigame-bot/internal/core"
+	"minigame-bot/internal/core/logger"
+	domainUser "minigame-bot/internal/domain/user"
+	"minigame-bot/internal/locker"
+	repository "minigame-bot/internal/repo/user"
+	"strconv"
+	"time"
+
+	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
+)
+
+func UserProvider(locker locker.ILocker, userRepo repository.IUserRepository) func(ctx *th.Context, update telego.Update) error {
+	const OPERATION_NAME = "middleware::user_provider"
+	return func(ctx *th.Context, update telego.Update) error {
+		var userTelegramID int64
+		var firstName string
+		var lastName string
+		var username string
+		var privateChatID *int64
+		var groupChatID *int64
+		var inlineMessageID *string
+		var chatInstance *int64
+		if update.Message != nil {
+			userTelegramID = update.Message.From.ID
+			firstName = update.Message.From.FirstName
+			lastName = update.Message.From.LastName
+			username = update.Message.From.Username
+			if update.Message.Chat.Type == "private" {
+				privateChatID = &update.Message.Chat.ID
+			} else {
+				groupChatID = &update.Message.Chat.ID
+			}
+		} else if update.CallbackQuery != nil {
+			userTelegramID = update.CallbackQuery.From.ID
+			firstName = update.CallbackQuery.From.FirstName
+			lastName = update.CallbackQuery.From.LastName
+			username = update.CallbackQuery.From.Username
+			if update.CallbackQuery.InlineMessageID != "" {
+				inlineMessageID = &update.CallbackQuery.InlineMessageID
+			}
+			if update.CallbackQuery.Message != nil && update.CallbackQuery.Message.IsAccessible() {
+				chat := update.CallbackQuery.Message.GetChat()
+				if chat.Type == "private" {
+					privateChatID = &chat.ID
+				} else {
+					groupChatID = &chat.ID
+				}
+			}
+			parsedChatInstance, err := strconv.ParseInt(update.CallbackQuery.ChatInstance, 10, 64)
+			if err != nil {
+				return err
+			}
+			chatInstance = &parsedChatInstance
+		} else if update.InlineQuery != nil {
+			userTelegramID = update.InlineQuery.From.ID
+			firstName = update.InlineQuery.From.FirstName
+			lastName = update.InlineQuery.From.LastName
+			username = update.InlineQuery.From.Username
+			inlineMessageID = &update.InlineQuery.ID
+		} else {
+			return core.ErrInvalidUpdate
+		}
+
+		l := slog.With(slog.String(logger.OperationField, OPERATION_NAME))
+
+		rawCtx := ctx.Context()
+		rawCtx = logger.WithLogValue(rawCtx, logger.UserTelegramIDField, userTelegramID)
+		rawCtx = logger.WithLogValue(rawCtx, logger.UserFirstNameField, firstName)
+		rawCtx = logger.WithLogValue(rawCtx, logger.UserLastNameField, lastName)
+		rawCtx = logger.WithLogValue(rawCtx, logger.UserNameField, username)
+		rawCtx = logger.WithLogValue(rawCtx, logger.PrivateChatIDField, privateChatID)
+		rawCtx = logger.WithLogValue(rawCtx, logger.GroupChatIDField, groupChatID)
+		rawCtx = logger.WithLogValue(rawCtx, logger.InlineMessageIDField, inlineMessageID)
+		rawCtx = logger.WithLogValue(rawCtx, logger.ChatInstanceField, chatInstance)
+		ctx = ctx.WithContext(rawCtx)
+		l.DebugContext(ctx, "UserProvider middleware started")
+
+		var user domainUser.User
+		user, err := userRepo.UserByTelegramID(ctx, userTelegramID)
+		if err != nil {
+			if errors.Is(err, core.ErrUserNotFound) {
+				l.InfoContext(ctx, "User not found, creating new")
+				now := time.Now()
+				buildedUser, err := domainUser.NewBuilder().
+					NewID().
+					TelegramIDFromInt(userTelegramID).
+					FirstName(domainUser.FirstName(firstName)).
+					LastName(domainUser.LastName(lastName)).
+					Username(domainUser.Username(username)).
+					ChatIDFromPointer(privateChatID).
+					CreatedAt(now).
+					UpdatedAt(now).
+					Build()
+				if err != nil {
+					return err
+				}
+				err = userRepo.CreateUser(ctx, buildedUser)
+				if err != nil {
+					return err
+				}
+				l.InfoContext(ctx, "User created")
+				user = buildedUser
+			} else {
+				return err
+			}
+		}
+
+		rawCtx = logger.WithLogValue(rawCtx, logger.UserIDField, user.ID().String())
+		ctx = ctx.WithContext(rawCtx)
+		ctx = ctx.WithValue(core.ContextKeyUser, user)
+
+		l.DebugContext(ctx, "Trying to lock user")
+		err = locker.Lock(user.ID())
+		if err != nil {
+			return err
+		}
+		l.DebugContext(ctx, "User locked")
+
+		updateErr := ctx.Next(update)
+		lockerErr := locker.Unlock(user.ID())
+		l.DebugContext(ctx, "User unlocked")
+		if lockerErr != nil {
+			return err
+		}
+		if updateErr != nil {
+			return err
+		}
+
+		return nil
+	}
+}
