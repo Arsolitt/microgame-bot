96cd2a462c8381751ff2382756f3daf57556c17d
Arsolitt <arsolitt@gmail.com>
2025-12-12 18:43:58 +0300

    Refactor user methods for improved readability and conciseness
    Simplify the User struct methods by condensing their implementation into single-line return statements. This change enhances code clarity and maintainability while preserving the existing functionality of user data retrieval.

--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -44,39 +44,11 @@ func NewUser(opts ...UserOpt) (User, error) {
 	return *u, nil
 }
 
-func (u User) ID() ID {
-	return u.id
-}
-
-func (u User) TelegramID() TelegramID {
-	return u.telegramID
-}
-
-func (u User) ChatID() *ChatID {
-	return u.chatID
-}
-
-func (u User) FirstName() FirstName {
-	return u.firstName
-}
-
-func (u User) LastName() LastName {
-	return u.lastName
-}
-
-func (u User) Username() Username {
-	return u.username
-}
-
-func (u User) CreatedAt() time.Time {
-	return u.createdAt
-}
-
-func (u User) UpdatedAt() time.Time {
-	return u.updatedAt
-}
-
-func (u User) UpdateTimestamp() User {
-	u.updatedAt = time.Now()
-	return u
-}
+func (u User) ID() ID                 { return u.id }
+func (u User) TelegramID() TelegramID { return u.telegramID }
+func (u User) ChatID() *ChatID        { return u.chatID }
+func (u User) FirstName() FirstName   { return u.firstName }
+func (u User) LastName() LastName     { return u.lastName }
+func (u User) Username() Username     { return u.username }
+func (u User) CreatedAt() time.Time   { return u.createdAt }
+func (u User) UpdatedAt() time.Time   { return u.updatedAt }
