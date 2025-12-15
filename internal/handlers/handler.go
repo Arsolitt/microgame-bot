e90c753eadeb144dc132a6909cc016d60aa50306
Arsolitt <arsolitt@gmail.com>
2025-12-15 19:14:06 +0300

    Add RPS game handling functions for callback data extraction and keyboard generation
    This commit introduces new functions to handle RPS game interactions, including extracting game IDs and choices from callback data, as well as building the game board keyboard for inline interactions. It enhances error management by defining a specific error for invalid callback data, contributing to improved robustness in RPS game processing and user interaction within the application.

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -2,9 +2,20 @@ package handlers
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"microgame-bot/internal/core"
 	"microgame-bot/internal/domain"
+	"microgame-bot/internal/domain/rps"
 	domainUser "microgame-bot/internal/domain/user"
+	"microgame-bot/internal/utils"
+	"strings"
+
+	"github.com/mymmrac/telego"
+)
+
+var (
+	ErrInvalidCallbackData = errors.New("invalid callback data")
 )
 
 func inlineMessageIDFromContext(ctx context.Context) (domain.InlineMessageID, error) {
@@ -24,4 +35,59 @@ func userFromContext(ctx context.Context) (domainUser.User, error) {
 		return domainUser.User{}, core.ErrUserNotFoundInContext
 	}
 	return user, nil
+}
+
+func extractGameID[ID utils.UUIDBasedID](callbackData string) (ID, error) {
+	parts := strings.Split(callbackData, "::")
+	if len(parts) < 4 {
+		var zero ID
+		return zero, ErrInvalidCallbackData
+	}
+	id, err := utils.UUIDFromString[ID](parts[3])
+	if err != nil {
+		var zero ID
+		return zero, err
+	}
+	return id, nil
+}
+
+func buildRPSGameBoardKeyboard(game *rps.RPS) *telego.InlineKeyboardMarkup {
+	rows := make([][]telego.InlineKeyboardButton, 0, 3)
+	if game.IsFinished() {
+		return &telego.InlineKeyboardMarkup{
+			InlineKeyboard: rows,
+		}
+	}
+
+	choices := []rps.Choice{rps.ChoiceRock, rps.ChoicePaper, rps.ChoiceScissors}
+
+	for _, choice := range choices {
+		icon := choice.Icon()
+		callbackData := fmt.Sprintf("g::rps::choice::%s::%s", game.ID().String(), choice.String())
+
+		button := telego.InlineKeyboardButton{
+			Text:         icon,
+			CallbackData: callbackData,
+		}
+		rows = append(rows, []telego.InlineKeyboardButton{button})
+	}
+
+	return &telego.InlineKeyboardMarkup{
+		InlineKeyboard: rows,
+	}
+}
+
+func extractRPSChoice(callbackData string) (rps.Choice, error) {
+	parts := strings.Split(callbackData, "::")
+	if len(parts) < 5 {
+		return rps.ChoiceEmpty, ErrInvalidCallbackData
+	}
+
+	var choice rps.Choice
+	_, err := fmt.Sscanf(parts[4], "%s", &choice)
+	if err != nil {
+		return rps.ChoiceEmpty, err
+	}
+
+	return choice, nil
 }
