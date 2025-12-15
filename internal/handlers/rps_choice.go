49fab488640aa7f1ba4ddf8e2fffe007874bb016
Arsolitt <arsolitt@gmail.com>
2025-12-15 19:14:14 +0300

    Refactor RPS choice handling to improve error management and streamline game logic
    This commit refactors the RPS choice handling function to enhance error reporting by including operation context in error messages. It updates the function signature to utilize a unit of work pattern for better transaction management and simplifies the retrieval of user and game session data. Additionally, it introduces a new message format for displaying round results and manages the creation of new games for subsequent rounds, contributing to a more robust and maintainable RPS game flow.

--- internal/handlers/rps_choice.go
+++ internal/handlers/rps_choice.go
@@ -1,58 +1,67 @@
 package handlers
 
 import (
-	"errors"
 	"fmt"
 	"log/slog"
-	"microgame-bot/internal/core"
+	"microgame-bot/internal/core/logger"
 	"microgame-bot/internal/domain"
 	"microgame-bot/internal/domain/gs"
 	"microgame-bot/internal/domain/rps"
-	domainUser "microgame-bot/internal/domain/user"
 	"microgame-bot/internal/msgs"
 	gsRepository "microgame-bot/internal/repo/gs"
 	rpsRepository "microgame-bot/internal/repo/rps"
 	userRepository "microgame-bot/internal/repo/user"
-	"strings"
+	"microgame-bot/internal/uow"
 
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
 )
 
-func RPSMove(gameRepo rpsRepository.IRPSRepository, userRepo userRepository.IUserRepository, gsRepo gsRepository.IGSRepository) CallbackQueryHandlerFunc {
+func RPSChoice(userGetter userRepository.IUserGetter, unit uow.IUnitOfWork) CallbackQueryHandlerFunc {
+	const OPERATION_NAME = "handler::rps_choice"
 	return func(ctx *th.Context, query telego.CallbackQuery) (IResponse, error) {
-		slog.DebugContext(ctx, "Move callback received")
+		slog.DebugContext(ctx, "RPS Choice callback received", logger.OperationField, OPERATION_NAME)
 
-		player, ok := ctx.Value(core.ContextKeyUser).(domainUser.User)
-		if !ok {
-			return nil, core.ErrUserNotFoundInContext
-		}
-
-		choice, err := extractChoice(query.Data)
+		player, err := userFromContext(ctx)
 		if err != nil {
-			return nil, fmt.Errorf("failed to extract choice: %w", err)
+			return nil, fmt.Errorf("failed to get user from context in %s: %w", OPERATION_NAME, err)
 		}
 
-		game, ok := ctx.Value(core.ContextKeyGame).(rps.RPS)
-		if !ok {
-			slog.ErrorContext(ctx, "Game not found")
-			return nil, core.ErrGameNotFoundInContext
-		}
-
-		session, ok := ctx.Value(core.ContextKeyGameSession).(gs.GameSession)
-		if !ok {
-			slog.ErrorContext(ctx, "Game session not found")
-			return nil, core.ErrGameSessionNotFoundInContext
-		}
-
-		game, err = game.MakeChoice(player.ID(), choice)
+		choice, err := extractRPSChoice(query.Data)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to extract choice in %s: %w", OPERATION_NAME, err)
 		}
 
-		game, err = gameRepo.UpdateGame(ctx, game)
+		gameID, err := extractGameID[rps.ID](query.Data)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to extract game ID from callback data in %s: %w", OPERATION_NAME, err)
+		}
+
+		var game rps.RPS
+		err = unit.Do(ctx, func(uow uow.IUnitOfWork) error {
+			gameRepo, err := uow.RPSRepo()
+			if err != nil {
+				return fmt.Errorf("failed to get game repository in %s: %w", OPERATION_NAME, err)
+			}
+			game, err = gameRepo.GameByIDLocked(ctx, gameID)
+			if err != nil {
+				return fmt.Errorf("failed to get game by ID with lock in %s: %w", OPERATION_NAME, err)
+			}
+
+			game, err = game.MakeChoice(player.ID(), choice)
+			if err != nil {
+				return fmt.Errorf("failed to make choice in %s: %w", OPERATION_NAME, err)
+			}
+
+			game, err = gameRepo.UpdateGame(ctx, game)
+			if err != nil {
+				return fmt.Errorf("failed to update game in %s: %w", OPERATION_NAME, err)
+			}
+
+			return nil
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed do transaction in %s: %w", OPERATION_NAME, err)
 		}
 
 		if !game.IsFinished() {
@@ -64,7 +73,24 @@ func RPSMove(gameRepo rpsRepository.IRPSRepository, userRepo userRepository.IUse
 			}, nil
 		}
 
-		allGames, err := gameRepo.GamesBySessionID(ctx, session.ID())
+		var gsGetter gsRepository.IGSGetter
+		gsGetter, err = unit.GSRepo()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get game session repository in %s: %w", OPERATION_NAME, err)
+		}
+
+		session, err := gsGetter.GameSessionByID(ctx, game.GameSessionID())
+		if err != nil {
+			return nil, fmt.Errorf("failed to get game session by ID in %s: %w", OPERATION_NAME, err)
+		}
+
+		var gameGetter rpsRepository.IRPSGetter
+		gameGetter, err = unit.RPSRepo()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get game repository in %s: %w", OPERATION_NAME, err)
+		}
+
+		allGames, err := gameGetter.GamesBySessionID(ctx, session.ID())
 		if err != nil {
 			return nil, fmt.Errorf("failed to get games by session ID: %w", err)
 		}
@@ -77,210 +103,153 @@ func RPSMove(gameRepo rpsRepository.IRPSRepository, userRepo userRepository.IUse
 		manager := gs.NewSessionManager(session, games)
 		result := manager.CalculateResult()
 
-		player1, err := userRepo.UserByID(ctx, game.Player1ID())
+		player1, err := userGetter.UserByID(ctx, game.Player1ID())
 		if err != nil {
 			return nil, err
 		}
 
-		player2, err := userRepo.UserByID(ctx, game.Player2ID())
+		player2, err := userGetter.UserByID(ctx, game.Player2ID())
 		if err != nil {
 			return nil, err
 		}
 
 		if result.IsCompleted {
-			session, err = session.ChangeStatus(domain.GameStatusFinished)
+			err = unit.Do(ctx, func(uow uow.IUnitOfWork) error {
+				gsRepo, err := uow.GSRepo()
+				if err != nil {
+					return fmt.Errorf("failed to get game session repository in %s: %w", OPERATION_NAME, err)
+				}
+				session, err = session.ChangeStatus(domain.GameStatusFinished)
+				if err != nil {
+					return fmt.Errorf("failed to change status of game session: %w", err)
+				}
+				session, err = gsRepo.UpdateGameSession(ctx, session)
+				if err != nil {
+					return fmt.Errorf("failed to update game session: %w", err)
+				}
+
+				return nil
+			})
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("failed do transaction in %s: %w", OPERATION_NAME, err)
 			}
 
-			session, err = gsRepo.UpdateGameSession(ctx, session)
-			if err != nil {
-				return nil, fmt.Errorf("failed to update game session: %w", err)
+			var winnerUsername string
+			if result.SeriesWinner == player1.ID() {
+				winnerUsername = string(player1.Username())
+			} else {
+				winnerUsername = string(player2.Username())
 			}
 
-			return handleSeriesCompleted(query, &game, player1, player2, result)
+			msg := fmt.Sprintf(
+				"🎮 <b>Серия завершена!</b>\n\n"+
+					"Счёт: %d - %d\n"+
+					"Ничьих: %d\n\n"+
+					"🏆 <b>Победитель серии:</b> @%s",
+				result.Scores[player1.ID()],
+				result.Scores[player2.ID()],
+				result.Draws,
+				winnerUsername,
+			)
+
+			return ResponseChain{
+				&EditMessageTextResponse{
+					InlineMessageID: query.InlineMessageID,
+					Text:            msg,
+					ParseMode:       "HTML",
+				},
+				&CallbackQueryResponse{
+					CallbackQueryID: query.ID,
+					Text:            fmt.Sprintf("🎉 Серия завершена! Победил @%s", winnerUsername),
+				},
+			}, nil
 		}
 
 		if result.NeedsNewRound {
-			return handleNewRound(ctx, query, &game, player1, player2, result, gameRepo, session)
+			var nextGame rps.RPS
+			err = unit.Do(ctx, func(uow uow.IUnitOfWork) error {
+				gameRepo, err := uow.RPSRepo()
+				if err != nil {
+					return fmt.Errorf("failed to get game repository in %s: %w", OPERATION_NAME, err)
+				}
+				nextGame, err := rps.New(
+					rps.WithNewID(),
+					rps.WithGameSessionID(session.ID()),
+					rps.WithCreatorID(game.CreatorID()),
+					rps.WithPlayer1ID(game.Player1ID()),
+					rps.WithPlayer2ID(game.Player2ID()),
+					rps.WithStatus(domain.GameStatusInProgress),
+				)
+				if err != nil {
+					return fmt.Errorf("failed to create new game in %s: %w", OPERATION_NAME, err)
+				}
+
+				nextGame, err = gameRepo.CreateGame(ctx, nextGame)
+				if err != nil {
+					return fmt.Errorf("failed to store new game in %s: %w", OPERATION_NAME, err)
+				}
+
+				return nil
+			})
+			if err != nil {
+				return nil, fmt.Errorf("failed do transaction in %s: %w", OPERATION_NAME, err)
+			}
+
+			msg := fmt.Sprintf(
+				"<b>Раунд завершен!</b>\n\n"+
+					"Текущий счёт:\n"+
+					"@%s: %d\n"+
+					"@%s: %d\n"+
+					"Ничьих: %d\n\n"+
+					"🎮 Начинаем следующий раунд!",
+				player1.Username(), result.Scores[player1.ID()],
+				player2.Username(), result.Scores[player2.ID()],
+				result.Draws,
+			)
+
+			keyboard := buildRPSGameBoardKeyboard(&nextGame)
+
+			return ResponseChain{
+				&EditMessageTextResponse{
+					InlineMessageID: query.InlineMessageID,
+					Text:            msg,
+					ParseMode:       "HTML",
+					ReplyMarkup:     keyboard,
+				},
+				&CallbackQueryResponse{
+					CallbackQueryID: query.ID,
+					Text:            getSuccessMessage(game),
+				},
+			}, nil
+		}
+		// Показываем результат текущего раунда
+		msg, err := msgs.RPSFinished(&game, player1, player2)
+		if err != nil {
+			return nil, err
 		}
 
-		return handleCurrentRoundResult(query, &game, player1, player2, result)
+		// Добавляем текущий счет
+		msg += fmt.Sprintf(
+			"\n\nТекущий счёт:\n"+
+				"@%s: %d\n"+
+				"@%s: %d\n"+
+				"Ничьих: %d",
+			player1.Username(), result.Scores[player1.ID()],
+			player2.Username(), result.Scores[player2.ID()],
+			result.Draws,
+		)
 
-		// boardKeyboard := buildRPSGameBoardKeyboard(&game)
-
-		// if game.IsFinished() {
-		// 	msg, err := msgs.RPSFinished(&game, player1, player2)
-		// 	if err != nil {
-		// 		return nil, err
-		// 	}
-
-		// 	return ResponseChain{
-		// 		&EditMessageTextResponse{
-		// 			InlineMessageID: query.InlineMessageID,
-		// 			Text:            msg,
-		// 			ParseMode:       "HTML",
-		// 			ReplyMarkup:     boardKeyboard,
-		// 		},
-		// 		&CallbackQueryResponse{
-		// 			CallbackQueryID: query.ID,
-		// 			Text:            getSuccessMessage(&game),
-		// 		},
-		// 	}, nil
-		// }
-
-		// return ResponseChain{
-		// 	&CallbackQueryResponse{
-		// 		CallbackQueryID: query.ID,
-		// 		Text:            getSuccessMessage(&game),
-		// 	},
-		// }, nil
+		return ResponseChain{
+			&EditMessageTextResponse{
+				InlineMessageID: query.InlineMessageID,
+				Text:            msg,
+				ParseMode:       "HTML",
+				ReplyMarkup:     buildRPSGameBoardKeyboard(&game),
+			},
+			&CallbackQueryResponse{
+				CallbackQueryID: query.ID,
+				Text:            getSuccessMessage(game),
+			},
+		}, nil
 	}
-}
-
-func extractChoice(callbackData string) (rps.Choice, error) {
-	parts := strings.Split(callbackData, "::")
-	if len(parts) < 5 {
-		return rps.ChoiceEmpty, errors.New("invalid callback data")
-	}
-
-	var choice rps.Choice
-	_, err := fmt.Sscanf(parts[4], "%s", &choice)
-	if err != nil {
-		return rps.ChoiceEmpty, err
-	}
-
-	return choice, nil
-}
-
-func handleSeriesCompleted(
-	query telego.CallbackQuery,
-	game *rps.RPS,
-	player1, player2 domainUser.User,
-	result gs.SessionResult,
-) (IResponse, error) {
-	// Определяем победителя серии
-	var winnerUsername string
-	if result.SeriesWinner == player1.ID() {
-		winnerUsername = string(player1.Username())
-	} else {
-		winnerUsername = string(player2.Username())
-	}
-
-	msg := fmt.Sprintf(
-		"🎮 <b>Серия завершена!</b>\n\n"+
-			"Счёт: %d - %d\n"+
-			"Ничьих: %d\n\n"+
-			"🏆 <b>Победитель серии:</b> @%s",
-		result.Scores[player1.ID()],
-		result.Scores[player2.ID()],
-		result.Draws,
-		winnerUsername,
-	)
-
-	return ResponseChain{
-		&EditMessageTextResponse{
-			InlineMessageID: query.InlineMessageID,
-			Text:            msg,
-			ParseMode:       "HTML",
-		},
-		&CallbackQueryResponse{
-			CallbackQueryID: query.ID,
-			Text:            fmt.Sprintf("🎉 Серия завершена! Победил @%s", winnerUsername),
-		},
-	}, nil
-}
-
-func handleNewRound(
-	ctx *th.Context,
-	query telego.CallbackQuery,
-	currentGame *rps.RPS,
-	player1, player2 domainUser.User,
-	result gs.SessionResult,
-	gameRepo rpsRepository.IRPSRepository,
-	session gs.GameSession,
-) (IResponse, error) {
-	// Создаем новую игру для следующего раунда
-	nextGame, err := rps.New(
-		rps.WithNewID(),
-		rps.WithGameSessionID(session.ID()),
-		rps.WithCreatorID(currentGame.CreatorID()),
-		rps.WithPlayer1ID(currentGame.Player1ID()),
-		rps.WithPlayer2ID(currentGame.Player2ID()),
-		rps.WithStatus(domain.GameStatusInProgress),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	nextGame, err = gameRepo.CreateGame(ctx, nextGame)
-	if err != nil {
-		return nil, err
-	}
-
-	// Формируем сообщение с текущим счетом
-	msg := fmt.Sprintf(
-		"<b>Раунд завершен!</b>\n\n"+
-			"Текущий счёт:\n"+
-			"@%s: %d\n"+
-			"@%s: %d\n"+
-			"Ничьих: %d\n\n"+
-			"🎮 Начинаем следующий раунд!",
-		player1.Username(), result.Scores[player1.ID()],
-		player2.Username(), result.Scores[player2.ID()],
-		result.Draws,
-	)
-
-	// Отрисовываем клавиатуру для новой игры
-	keyboard := buildRPSGameBoardKeyboard(&nextGame)
-
-	return ResponseChain{
-		&EditMessageTextResponse{
-			InlineMessageID: query.InlineMessageID,
-			Text:            msg,
-			ParseMode:       "HTML",
-			ReplyMarkup:     keyboard,
-		},
-		&CallbackQueryResponse{
-			CallbackQueryID: query.ID,
-			Text:            getSuccessMessage(currentGame),
-		},
-	}, nil
-}
-
-func handleCurrentRoundResult(
-	query telego.CallbackQuery,
-	game *rps.RPS,
-	player1, player2 domainUser.User,
-	result gs.SessionResult,
-) (IResponse, error) {
-	// Показываем результат текущего раунда
-	msg, err := msgs.RPSFinished(game, player1, player2)
-	if err != nil {
-		return nil, err
-	}
-
-	// Добавляем текущий счет
-	msg += fmt.Sprintf(
-		"\n\nТекущий счёт:\n"+
-			"@%s: %d\n"+
-			"@%s: %d\n"+
-			"Ничьих: %d",
-		player1.Username(), result.Scores[player1.ID()],
-		player2.Username(), result.Scores[player2.ID()],
-		result.Draws,
-	)
-
-	return ResponseChain{
-		&EditMessageTextResponse{
-			InlineMessageID: query.InlineMessageID,
-			Text:            msg,
-			ParseMode:       "HTML",
-			ReplyMarkup:     buildRPSGameBoardKeyboard(game),
-		},
-		&CallbackQueryResponse{
-			CallbackQueryID: query.ID,
-			Text:            getSuccessMessage(game),
-		},
-	}, nil
 }
