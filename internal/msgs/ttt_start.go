96dbca0ce23bb41ecc854a88d7125cafb6ee46af
Arsolitt <arsolitt@gmail.com>
2025-12-18 20:49:03 +0300

    Refactor TTT game start and player messaging functions for improved clarity
    This commit updates the TTTStart and TTTGameStarted functions to simplify parameters and enhance player messaging. The logic now clearly distinguishes between the game creator and players, providing more informative messages about game initiation and player status. These changes improve the overall user experience in the microgame-bot application.

--- internal/msgs/ttt_start.go
+++ internal/msgs/ttt_start.go
@@ -7,27 +7,30 @@ import (
 	"strings"
 )
 
-func TTTStart(user domainUser.User, game ttt.TTT) (string, error) {
+func TTTStart(creator domainUser.User) (string, error) {
 	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("@%s ", user.Username()))
+	sb.WriteString(fmt.Sprintf("@%s ", creator.Username()))
 	sb.WriteString("запустил игру <b>крестики-нолики</b>")
-	sb.WriteString("\n")
-	sb.WriteString(fmt.Sprintf("👤 @%s %s", user.Username(), game.PlayerCell(user.ID()).Icon()))
+	sb.WriteString("\n\n")
+	sb.WriteString("👤 <i>Ожидание игроков...</i>")
+
+	return sb.String(), nil
+}
+
+func TTTFirstPlayerJoined(creator domainUser.User, firstPlayer domainUser.User) (string, error) {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("@%s ", creator.Username()))
+	sb.WriteString("запустил игру <b>крестики-нолики</b>")
+	sb.WriteString("\n\n")
+	sb.WriteString(fmt.Sprintf("👤 @%s %s", firstPlayer.Username(), ttt.CellEmptyIcon))
 	sb.WriteString("\n")
 	sb.WriteString("👤 <i>Ожидание второго игрока...</i>")
 
 	return sb.String(), nil
 }
 
-func TTTGameStarted(game *ttt.TTT, playerX domainUser.User, playerO domainUser.User) (string, error) {
+func TTTGameStarted(creator domainUser.User, playerX domainUser.User, playerO domainUser.User) (string, error) {
 	var sb strings.Builder
-
-	var creator domainUser.User
-	if game.CreatorID() == playerX.ID() {
-		creator = playerX
-	} else {
-		creator = playerO
-	}
 
 	sb.WriteString(fmt.Sprintf("@%s ", creator.Username()))
 	sb.WriteString("запустил игру <b>крестики-нолики</b>")
